c0da6d69d4bd6eaca008def2edf6108bea47f2e8
ZLM <2479326918@qq.com>
2022-08-08 15:59:59 +0800

    向数据库添加数据操作

--- Nodejs/4、基于 MySQL 数据库的后端数据持久化/课件/代码/app1/testMiao2.js
+++ Nodejs/4、基于 MySQL 数据库的后端数据持久化/课件/代码/app1/testMiao2.js
@@ -46,12 +46,12 @@ router.get('/users', async (ctx,next)=>{
     * [ [], [] ]
     * 第一个数据：查询出来的所有的数据的集合,集合中的数据被包装成了一个对象 TextRow ，和普通对象没有太大的区别
     * 第二个数组：是执行该sql对应的查询信息
+    * 例如：[ TextRow { id: 6, username: '里斯' },TextRow { id: 7, username: '章三' } ]
     * */
     let [users] = await connection.query(
         "SELECT * FROM `kkb-db-nodejs`.users;"
     );
-    console.log("res____", users)
-    //[ TextRow { id: 6, username: '里斯' },TextRow { id: 7, username: '章三' } ]
+    console.log("res____", users) //[ TextRow { id: 6, username: '里斯' },TextRow { id: 7, username: '章三' } ]
   
     ctx.body = nunjucks.render('users.html', {
         users
@@ -62,11 +62,40 @@ router.get('/users', async (ctx,next)=>{
 router.get('/add', async (ctx, next) => {
     ctx.body = nunjucks.render('add.html');
 });
+
+/**
+ * 如果query执行的是insert into，那么返回值就是一个数组：包含两项
+ * 1、数组中的第一个值的一个对象 ResultSetHeader
+ * [
+        ResultSetHeader {
+            fieldCount: 0,
+            affectedRows: 1, //插入的数据条数
+            insertId: 10,    //插入的数据当前的自增ID
+            info: '',
+            serverStatus: 2,
+            warningStatus: 0
+        },
+        undefined
+    ]
+    * */ 
+//向数据库插入数据
 router.post('/add', koaBody() ,async (ctx, next) => {
-    let {username, age} = ctx.request.body;
+    let {username, age, gender} = ctx.request.body;
     console.log("post/add______", ctx.request.body)
 
-    ctx.body = '提交数据'
+    let res = await connection.query(
+        //方法一：
+        // "insert into `users`(`username`) values("+username+") " //values变量值填充数据
+
+        //方法二：
+        // ?表示 数组中的每一个值，对应替换sql语句中每一个?
+        "insert into `users`(`username`, `age`, `gender`) values(?, ?, ?) ", //预处理
+        [username, age, gender]
+    )
+    console.log('res____add', res)
+
+    ctx.set('Content-Type', 'text/html;charset=utf-8');
+    ctx.body = '添加成功 <br> <a href="/add">继续添加</a> <br> <a href="/users">返回用户列表页面</a>';
 });
 app.use(router.routes())
 
