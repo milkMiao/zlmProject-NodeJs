678ed026160bf446437b9e9443e2bf76b231afd6
ZLM <2479326918@qq.com>
2022-08-24 17:05:05 +0800

    mysql编码

--- Nodejs深入(全栈)/第4节、MySQL/04/shop/backend/index.js
+++ Nodejs深入(全栈)/第4节、MySQL/04/shop/backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('koa-bodyparser')
 app.use(require('koa-static')(__dirname + '/'))
 app.use(bodyParser())
 
-// 初始化数据库
+// 初始化数据库 ER实体关系=领域模型关系
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
@@ -16,6 +16,7 @@ const OrderItem = require('./models/order-item');
 
 // 加载用户 - 代替鉴权
 app.use(async (ctx, next) => {
+    //虚拟鉴权
     const user = await User.findByPk(1)
     ctx.user = user;
     await next();
@@ -147,19 +148,24 @@ router.get('/orders', async ctx => {
 })
 
 
-app.use(router.routes())
+app.use(router.routes())//加载路由规则
 
 // app.use('/admin', adminRoutes.routes);
 // app.use(shopRoutes);
 
 
 Product.belongsTo(User, {
+    //外界依赖
+    // 下面方式称之为：数据库完整性约束
+    // 比方说User夏老师，创建几个商品--啤酒，饮料，八宝粥，当这个人离职了，那么要删除这个人，两者之间的关联关系不好确认
     constraints: true,
     onDelete: 'CASCADE'
 });
 User.hasMany(Product);
-User.hasOne(Cart);
+User.hasOne(Cart);//意味着：在User里面就 create，delete...等方法
 Cart.belongsTo(User);
+
+// 多对多
 Cart.belongsToMany(Product, {
     through: CartItem
 });
@@ -175,15 +181,17 @@ Product.belongsToMany(Order, {
     through: OrderItem
 });
 
+//同步
 sequelize.sync().then(
     async result => {
-        let user = await User.findByPk(1)
+        //设置初始数据（其实在模型设置），而不是在数据库初始化
+        let user = await User.findByPk(1)//查询 主键1的数据
         if (!user) {
             user = await User.create({
                 name: 'Sourav',
                 email: 'sourav.dey9@gmail.com'
             })
-            await user.createCart();
+            await user.createCart();//购物车（为啥有createCart方法？见164行）
         }
         app.listen(3000, () => console.log("Listening to port 3000"));
     })
