bdd29ea50a2dda3475454a15fc89a5695f04d43d
ZLM <2479326918@qq.com>
2023-11-22 09:00:05 +0800

    提交

---
+++ Nodejs入门/搭建一个 monorepo风格的脚手架工程/mortal-cli/index.js
@@ -0,0 +1,6 @@
+/**
+ * 步骤：
+ * 1、npm init 命令来初始化一个脚手架工程，生成一个package.json文件
+ * 2、在 pakeage.json 中添加 bin 字段，来声明一个命令 [注意在文件头部添加 #!/usr/bin/env node，否则运行后会报错！]
+ */
+console.log("搭建一个 monorepo 风格的脚手架工程");
